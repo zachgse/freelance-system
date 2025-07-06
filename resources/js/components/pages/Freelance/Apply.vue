dd348219daef6e73f5e61ef08d14bccd5681a27f
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-07-06 20:57:21 +0800

    Fix user interface on application proposal

--- resources/js/components/pages/Freelance/Apply.vue
+++ resources/js/components/pages/Freelance/Apply.vue
@@ -117,37 +117,35 @@ async function postApplyFreelance(){
         <div class="border-1 border-gray-300 w-full h-auto flex flex-col justify-center mx-auto my-12 p-4">
             <div class="grid grid-cols-12">
                 <div class="col-span-12">
-                    <p class="font-bold text-2xl">Job Details</p>
+                    <p class="font-bold text-xl">Job Details</p>
                     <div class="border-b border-gray-300 p-4">
-                        <p class="font-bold text-xl">{{ freelanceDetails?.title }}</p>
-                        <p class="text-sm text-gray-500 mt-2">{{new Date(freelanceDetails?.date_posted).toLocaleDateString("en-US", { year:'numeric',month:'long',day:'numeric' })}}</p>
+                        <p class="font-bold text-lg">{{ freelanceDetails?.title }}</p>
+                        <p class="text-xs text-gray-500 mt-2">{{new Date(freelanceDetails?.date_posted).toLocaleDateString("en-US", { year:'numeric',month:'long',day:'numeric' })}}</p>
                     </div>
                     <div class="border-b border-gray-300 p-4">
                         <p>{{ freelanceDetails?.description }}</p>
                     </div>
                     <div class="border-b border-gray-300 p-4">
-                        <div class="flex items-center gap-2 text-sm text-gray-500">
-                            <Tag/> Php {{ freelanceDetails?.rate }}
+                        <div class="flex items-center gap-2 text-sm text-gray-500 font-bold">
+                            <Tag class="w-4 h-4"/> Php {{ freelanceDetails?.rate }}
                         </div>
                     </div>
                     <div class="border-b border-gray-300 p-4">
-                        <p class="font-bold text-xl">Category</p>
-                        <div class="bg-blue-500 text-white text-center rounded-xl p-4 w-fit h-8 flex items-center mt-4">
-                            {{ freelanceDetails?.category }}
-                        </div>
+                        <p class="font-bold text-xl">Category</p> <br/>
+                        <span class="text-white py-1 px-10 rounded-full bg-blue-500 text-xs">{{ freelanceDetails?.category  }}</span>
                     </div>
                     <div class="border-b border-gray-300 flex flex-col gap-4 p-4">
                         <p class="font-bold text-xl">Activity</p>
-                        <p class="text-sm text-gray-500">Total Proposals: {{ freelanceDetails?.number_of_total_proposals }}</p>
-                        <p class="text-sm text-gray-500">Pending Proposals: {{ freelanceDetails?.number_of_pending_proposals }}</p>
-                        <p class="text-sm text-gray-500">Declined Proposals: {{ freelanceDetails?.number_of_declined_proposals }}</p>
+                        <p class="text-xs text-gray-500 font-bold">Total Proposals: <span class="font-normal">{{ freelanceDetails?.number_of_total_proposals }}</span></p>
+                        <p class="text-xs text-gray-500 font-bold">Pending Proposals: <span class="font-normal">{{ freelanceDetails?.number_of_pending_proposals }}</span></p>
+                        <p class="text-xs text-gray-500 font-bold">Declined Proposals: <span class="font-normal">{{ freelanceDetails?.number_of_declined_proposals }}</span></p>
                     </div>
                     <p class="font-bold text-2xl mt-4">Client Details</p>
                     <div class="px-4 mt-4">
-                        <p class="text-xl font-bold text-gray-500">{{ clientDetails?.client_name }}</p>
+                        <p class="text-lg font-bold text-gray-500">{{ clientDetails?.client_name }}</p>
                     </div>
                     <div class="px-4 mt-4">
-                        <p class="text-sm font-bold text-gray-500">{{ clientDetails?.number_of_total_projects }} job/s posted</p>
+                        <p class="text-xs font-bold text-gray-500">{{ clientDetails?.number_of_total_projects }} job/s posted</p>
                     </div>
                     <div class="px-4 mt-4">
                         <p class="text-xs text-gray-500">Member since {{new Date(clientDetails?.date_registered).toLocaleDateString("en-US", { year:'numeric',month:'long',day:'numeric' })}}</p>
