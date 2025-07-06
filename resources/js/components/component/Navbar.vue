40e70d045fd970a60d9aad3a300ea11352491019
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-07-06 20:22:28 +0800

    Add dropdown toggler on navbar and fix Index User Interface

--- resources/js/components/component/Navbar.vue
+++ resources/js/components/component/Navbar.vue
@@ -1,11 +1,23 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { ref,watch} from 'vue';
+import { useRoute,useRouter } from 'vue-router';
 import { useAuthStore } from '../../authStore';
-import { MessageSquareMore } from 'lucide-vue-next';
+import { MessageSquareMore, ChevronDown, Send } from 'lucide-vue-next';
 import Swal from 'sweetalert2';
 
+const route = useRoute();
 const router = useRouter();
 const authStore = useAuthStore();
+
+const isDropdownOpen = ref(false);
+
+watch(() => route.fullPath, () => {
+  isDropdownOpen.value = false;
+});
+
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+}
 
 const logout = async () => {
   const result = await Swal.fire({ //swal confirmation modal 
@@ -44,27 +56,39 @@ const logout = async () => {
 <template>
 <div class="w-full border-b border-gray-300 py-4">
     <div class="max-w-[1300px] w-full flex items-center justify-between mx-auto px-2">
-        <router-link to="/" class="text-5xl uppercase font-extrabold text-blue-500 ">Freelancy</router-link>
-        <div v-if="authStore.isAuthenticated" class="flex gap-4">
-            <router-link :to="{name:'inbox'}">
-                <MessageSquareMore/>
+        <router-link to="/" class="text-4xl uppercase font-extrabold text-blue-500 ">Freelancy</router-link> 
+        <div v-if="authStore.isAuthenticated" class="flex flex gap-4 relative">
+            <router-link :to="{name:'inbox'}" class="mt-1">
+                <MessageSquareMore class="text-blue-500 h-6 w-6"/>
             </router-link>
-            <div v-if="authStore.getUser.user_type == 'client'">
-                <router-link :to="{name:'client-view-projects'}">Projects</router-link>
+            <p class="cursor-pointer flex items-center gap-1 text-sm" @click="toggleDropdown()">
+              {{authStore?.getUser.name}} <span><ChevronDown class="w-3 h-3 mt-1"/></span>
+            </p>
+            <div v-if="isDropdownOpen" 
+              class="absolute right-0 top-10 z-50 bg-white border border-gray-300 rounded w-40 max-h-auto text-center text-xs">
+              <div class="border-b border-gray-300 cursor-pointer hover:bg-gray-100 py-2">
+                <router-link :to="{name:'edit-profile'}">
+                  Profile
+                </router-link>
+              </div>
+              <div v-if="authStore.getUser.user_type == 'freelancer'" class="border-b border-gray-300 cursor-pointer hover:bg-gray-100 py-2">
+                <router-link :to="{name:'freelancer-view-proposals'}">
+                  Proposals
+                </router-link>
+              </div>
+              <div v-if="authStore.getUser.user_type == 'client'" class="border-b border-gray-300 cursor-pointer hover:bg-gray-100 py-2">
+                <router-link :to="{name:'client-view-projects'}">
+                  Projects
+                </router-link>
+              </div>    
+              <div class="cursor-pointer cursor-pointer hover:bg-gray-100 py-2 text-red-500" @click="logout()">Logout</div>
             </div>
-            <div v-if="authStore.getUser.user_type == 'freelancer'">
-                <router-link :to="{name:'freelancer-view-proposals'}">Proposals</router-link>
-            </div>
-            <router-link :to="{name:'edit-profile'}">
-                Profile
-            </router-link>
-            <p>Hi {{ authStore.getUser.name }}</p>
-            <p class="cursor-pointer" @click="logout()">Logout</p>
         </div>
         <div v-else>
             <router-link to="/login" class="">Login</router-link>
         </div>
-        
+
     </div>
+      
 </div>
 </template>
--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -102,7 +102,7 @@ async function fetchFreelances() {
     </div>
 
     <div v-else>
-        <div class="relative my-24">
+        <div class="relative my-12">
             <InputBox class="w-full h-12 px-8" placeholder="Search..." v-model="queryParams"/>
             <Search class="absolute right-4 top-3 cursor-pointer" @click="fetchFreelancesDebounced(queryParams)"></Search>
         </div>
@@ -113,7 +113,7 @@ async function fetchFreelances() {
 
         <div v-else>
             <div class="flex justify-between items-center">
-                <div class="my-12">
+                <div class="my-6">
                     <select class="bg-gray-100 w-full h-12 px-8 rounded-lg cursor-pointer" @change="sortParams = $event.target.value">
                         <option value="1">Latest</option> <!-- LATEST ORDER BY DESC > 1 --> 
                         <option value="2">Oldest</option> <!-- OLDEST ORDER BY ASC > 2 -->
@@ -121,7 +121,7 @@ async function fetchFreelances() {
                         <option value="4">High to Low (Rate)</option> <!-- HIGHEST ORDER BY DESC 4 -->
                     </select>
                 </div>
-                <div class="flex my-12 gap-2">
+                <div class="flex my-6 gap-2">
                     <div v-for="(n, index) in numberOfPage" :key="index"
                         :class="{
                             'h-12 w-12 bg-gray-100 flex items-center justify-center cursor-pointer': true,
@@ -137,21 +137,21 @@ async function fetchFreelances() {
 
         <router-link :to="{name: 'freelance-details' , params: {slug:freelance?.freelance_project_details?.slug}}" v-for="(freelance, index) in freelances" :key="freelance?.id"
             :class="{
-                'border-b border-t border-gray-300 w-full h-auto flex flex-col justify-center mx-auto gap-2 px-2 py-4 my-4': true,
+                'border-b border-t border-gray-300 w-full h-auto flex flex-col justify-center mx-auto gap-2 px-2 py-4 hover:opacity-90': true,
                 'bg-gray-100': index % 2 != 0
             }">
             
                 <div class="flex items-center justify-between">
-                    <p class="text-bold text-2xl">{{ freelance?.freelance_project_details?.title }}</p>
-                    <p class="text-sm text-gray-500">{{new Date(freelance?.freelance_project_details?.date_posted).toLocaleDateString("en-US", { year:'numeric',month:'long',day:'numeric' })}}</p>
+                    <p class="text-bold text-xl">{{ freelance?.freelance_project_details?.title }}</p>
+                    <p class="text-xs text-gray-500">{{new Date(freelance?.freelance_project_details?.date_posted).toLocaleDateString("en-US", { year:'numeric',month:'long',day:'numeric' })}}</p>
                 </div>
                 
-                <p class="text-sm text-gray-500">Rate: Php {{ freelance?.freelance_project_details?.rate }}</p>
+                <p class="text-xs text-gray-500">Rate: Php {{ freelance?.freelance_project_details?.rate }}</p>
                 <p>{{ freelance?.description }}</p>
                 <div class="bg-blue-500 text-white text-center rounded-xl p-4 w-fit h-8 flex items-center">
                     {{ freelance?.freelance_project_details?.category }}
                 </div>
-                <p class="text-gray-500">Number of proposals: {{ freelance?.freelance_project_details?.number_of_total_proposals }}</p>
+                <p class="text-xs text-gray-500">Number of proposals: {{ freelance?.freelance_project_details?.number_of_total_proposals }}</p>
             
         </router-link>  
         
