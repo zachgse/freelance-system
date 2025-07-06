d203bee705bdb685df0f5fa32df241609db8fd65
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-07-06 14:56:23 +0800

    Fix pagination user interface on list of freelance projects for clients

--- resources/js/components/pages/Client/ViewProjects.vue
+++ resources/js/components/pages/Client/ViewProjects.vue
@@ -36,6 +36,7 @@ const savingMessage = ref(null);
 const isError = ref(false);
 
 //for pagination
+const totalItems = ref();
 const itemsPerPage = 10;
 const numberOfPage = ref();
 const currentPage = ref(parseInt(route.query.page)  || 1); //add url tracking
@@ -288,8 +289,8 @@ async function fetchFreelances(){
             withCredentials:true
         });
         freelances.value = response.data.data|| [];
-        const totalItems = freelances.value.length || 0;
-        numberOfPage.value = Math.max(1, Math.ceil(totalItems / itemsPerPage));
+        totalItems.value = freelances.value.length || 0;
+        numberOfPage.value = Math.max(1, Math.ceil(totalItems.value / itemsPerPage));
         if (route.query.page) {
             switchPage(parseInt(route.query.page));
         } 
@@ -492,32 +493,21 @@ async function updateStatusFreelance() {
                 </tr>
             </tbody>
         </table>
-        <!-- <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 p-5" aria-label="Table navigation">
-            <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span class="font-semibold text-gray-900 dark:text-white">1000</span></span>
-            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                <li>
-                    <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                </li>
-                <li>
-                    <a href="#" aria-current="page" class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                </li>
-                <li>
-                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 p-5" aria-label="Table navigation">
+            <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+                Showing <span class="font-semibold text-gray-900 dark:text-white">{{startCurrentPage+1}}-{{endCurrentPage}}</span>
+                of 
+                <span class="font-semibold text-gray-900 dark:text-white">{{ totalItems }}</span>
+            </span>
+            <ul v-if="freelancesPaginated.length > 0" class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                <li v-for="(page,index) in numberOfPage" :key="index" class="cursor-pointer">
+                    <a @click="switchPage(page)" :class="{'bg-gray-300' : parseInt(currentPage) === page}"
+                        class="flex items-center justify-center px-3 h-8 border border-gray-300 bg-blue-50 hover:opacity-70">
+                        {{page}}
+                    </a>
                 </li>
             </ul>
-        </nav> -->
+        </nav>
     </div>
 
     <div class="flex my-12 float-right gap-2">
